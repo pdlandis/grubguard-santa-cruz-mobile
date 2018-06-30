ac0631f720e1943f931767ac722669456a459344
pdlandis <patricklandis@gmail.com>
2018-06-30 13:02:34 -0700

    Added _id, score, grade fields to Facility class.

--- app/_objects/Facility.ts
+++ app/_objects/Facility.ts
@@ -1,8 +1,13 @@
 import { Geolocation } from "./Geolocation";
 
 export class Facility {
+  _id: string;
   name: string;
   address: string;
+
+  score: number;
+  grade: string;
+
   location: Geolocation;
   distance: number;
 }
