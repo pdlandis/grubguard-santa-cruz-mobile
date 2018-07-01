15b14fcb847e737a0c14e6e4dc91ac183e72970d
pdlandis <patricklandis@gmail.com>
2018-06-30 18:52:03 -0700

    Improved detail view styling

--- app/tabs/facility-detail/facility-detail.component.ts
+++ app/tabs/facility-detail/facility-detail.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { CanActivate, Resolve, ActivatedRoute, RouterStateSnapshot } from "@angular/router";
 import { PageRoute } from "nativescript-angular/router";
 import { switchMap } from "rxjs/operators";
-
-import { CanActivate, Resolve, ActivatedRoute, RouterStateSnapshot } from "@angular/router";
 
 import { Facility } from "../../_objects/Facility";
 import { Inspection } from "../../_objects/Inspection";
 import { FacilityService } from "../../_services/Facility.service";
 import { InspectionService } from "../../_services/Inspection.service";
-
-//import { LocalCacheService } from
 
 @Component({
   selector: "FacilityDetail",
@@ -17,8 +14,6 @@ import { InspectionService } from "../../_services/Inspection.service";
   templateUrl: "./facility-detail.component.html",
   providers: [
     InspectionService,
-    //FacilityService,
-    //LocalCacheService,
   ],
 })
 export class FacilityDetailComponent implements OnInit {
@@ -26,8 +21,6 @@ export class FacilityDetailComponent implements OnInit {
   private facility: Facility;
   private itemList: Inspection[];
   private isLoading: boolean;
-
-  //@Input() facilityInput: Facility;
 
   constructor(
     private route: ActivatedRoute,
@@ -39,14 +32,6 @@ export class FacilityDetailComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // console.log("pageRoute:");
-    // console.log(this.pageRoute);
-    // console.log("route:");
-    // console.log(this.route);
-    //
-    // console.log("route.snapshot:");
-    // console.log(this.route.snapshot);
-
     this.pageRoute.activatedRoute.pipe(
       switchMap(activatedRoute => activatedRoute.params)
     ).forEach((params) => {
@@ -70,4 +55,60 @@ export class FacilityDetailComponent implements OnInit {
         this.isLoading = false;
       });
   }
+
+  parseInspectionDate(inspection: Inspection): string {
+    return (new Date(inspection.date)).toLocaleDateString();
+  }
+
+  hasMajorViolations(item: Inspection): boolean {
+    return (item.violationsMajor !== '0');
+  }
+
+  hasMinorViolations(item: Inspection): boolean {
+    return (item.violationsMinor !== '0');
+  }
+
+  parseViolationCode(code: String): string {
+    switch(code) {
+      case 'EH':
+        return "Employee Hygiene & Training";
+      case 'FP':
+        return "Improper Food Handling Procedures";//"Improper Food Preparation / Handling Procedures";
+      case 'FS':
+        return "Unapproved or Contaminated Food";//"Unapproved Food Source / Contaminated/Adulterated Food";
+      case 'FT':
+        return "Improper Food Temperatures";//"Improper Food Holding / Processing Temperatures";
+      case 'HW':
+        return "Inadequate Hand Washing Procedure";
+      case 'VI':
+        return "Vermin (rodent or insect) Infestation";
+      case 'WS':
+        return "Inadequate Equipment Washing";//"Inadequate Utensil / Equipment Washing or Sanitizing";
+      default:
+        return "";
+    }
+  }
+
+  parseMajorViolations(inspection: Inspection): string[] {
+    let vlist = [];
+    for (let v of inspection.violationsMajor.split(' ')) {
+      if (v !== '0') {
+        vlist.push(`[MAJOR] ${this.parseViolationCode(v)}`);
+      }
+    }
+    return vlist;
+  }
+
+  parseMinorViolations(inspection: Inspection): string[] {
+    let vlist = [];
+    for (let v of inspection.violationsMinor.split(' ')) {
+      if (v !== '0') {
+        vlist.push(`[MINOR] ${this.parseViolationCode(v)}`);
+      }
+    }
+    return vlist;
+  }
+
+
+
 }
