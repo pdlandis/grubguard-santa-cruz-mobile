1a4fcbf65aaabdb5de3a88c8d2aace9748603277
pdlandis <patricklandis@gmail.com>
2018-07-17 16:29:33 -0700

    Added AWS Elastic IP address

--- app/_config/settings.ts
+++ app/_config/settings.ts
@@ -1,5 +1,3 @@
 export class Settings {
-  static API_BASE: string ="http://192.168.0.104:3000/api";
-  //static API_BASE: string = "" // use relative urls targeted at same domain
-  //static API_BASE: string = "http://54.67.81.235/api" // use test EC2 instance
+  static API_BASE: string = "http://54.241.41.194/api";
 }
