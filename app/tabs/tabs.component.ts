ab3e933b7a34d033f92d7cf942eb90a63b87f6f7
pdlandis <patricklandis@gmail.com>
2018-10-21 20:04:49 -0700

    Search tab no longer opens the keyboard on iOS

--- app/tabs/tabs.component.ts
+++ app/tabs/tabs.component.ts
@@ -54,14 +54,16 @@ export class TabsComponent implements OnInit {
 
         let currentOrientation = getOrientation();
         if (currentOrientation === 'portrait' && selectedTabViewItem.title === 'Search') {
-          // Focus the search bar if we're navigating to Search while in portrait mode
-          // (forcing the keyboard to appear in landscape breaks navigation flow)
-          let sb = <SearchBar>this.page.getViewById('sb');
-          sb.focus();
-
-          // Patch for an android issue where focusing the input doesn't bring up the keyboard.
-          // https://github.com/NativeScript/NativeScript/issues/1973#issuecomment-230395339
+          // On Android, automatically open the keyboard on search.
+          // This is skipped on iOS because the keyboard blocks the
           if (isAndroid) {
+            // Focus the search bar if we're navigating to Search while in portrait mode
+            // (forcing the keyboard to appear in landscape breaks navigation flow)
+            let sb = <SearchBar>this.page.getViewById('sb');
+            sb.focus();
+
+            // Patch for an android issue where focusing the input doesn't bring up the keyboard.
+            // https://github.com/NativeScript/NativeScript/issues/1973#issuecomment-230395339
             let imm = utils.ad.getInputMethodManager();
             if (imm) {
               imm.toggleSoftInput(android.view.inputmethod.InputMethodManager.SHOW_IMPLICIT, 0);
